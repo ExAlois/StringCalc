68d9b69ef761383f895103d7ea73d14c733cab43
ExAlois <laaasss@mail.ru>
2014-01-20 17:32:24 +0200

    lst

--- src/com/strincalc/Main.java
+++ src/com/strincalc/Main.java
@@ -4,11 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StringCalc c = new StringCalc();
-
-        int i = c.add("asdf1df8sf74;6,1", ",");
-        System.out.println(i);
-
-
     }
 }
